4be195bfca920b4cd39726c141c1993724968bd3
mathrip <mathilde.ripart@gmail.com>
2022-01-27 17:31:03 +0000

    add parallelisation defacing + verbose

--- scripts/meld_bidsify_data_step2.py
+++ scripts/meld_bidsify_data_step2.py
@@ -5,7 +5,10 @@ import json
 import argparse
 import pandas as pd
 import shutil
+import itertools
+from joblib import Parallel, delayed
 from datetime import datetime
+import logging
 
 
 #functions
@@ -72,6 +75,86 @@ def create_json_file(name, info):
     with open(name, 'w') as outfile:
         json.dump(dictionary, outfile)
 
+        
+def deface_and_sort_nii(T_mod, path, tmp_folder_participant):
+    T=T_mod[0]
+    mod=T_mod[1]
+    dcm_folder=os.path.join(path,T,mod)
+    if not os.path.isdir(dcm_folder):
+        print(f'folder {dcm_folder} does not exist. Check your participant folder is similar to the meld_template')
+    if not os.listdir(dcm_folder):
+        pass
+    else:
+        files_nii=glob.glob(os.path.join(dcm_folder,'*.nii*'))
+        files_json=glob.glob(os.path.join(dcm_folder,'*.json'))
+        files_bvec=glob.glob(os.path.join(dcm_folder,'*.bvec'))   # for dwi files
+        files_bval=glob.glob(os.path.join(dcm_folder,'*.bval'))   # for dwi files
+        name_base = '.'.join([T,mod])
+        name_nii = name_base+'.nii'
+        name_json = name_base+'.json'
+        #if nifti file : copy nifti in tmp_dcm2bids and create fake json file
+        if (len(files_nii)>1) & (mod!='dwi') :
+            print(f'WARNING: There should be only one nifti file for modality {mod} at {strenght}. Check again and remove additional file.')
+        elif len(files_nii)==0:
+            print(f'WARNING: No files found for modality {mod} at {strenght}. Skip')
+        else:
+            for f in files_nii:
+                if 'negPE' in f:
+                    name_nii = name_base+'_negPE'+'.nii'
+                    name_json = name_base+'_negPE'+'.json'
+                if args.nodeface==True:
+                    command = format(f'cp {f} {tmp_folder_participant}/{name_nii}')
+                    sub.check_call(command, shell=True)
+                    print(f'no deface. just copy nii {name_nii} in {tmp_folder_participant}')
+                else:
+                    command = format(f'pydeface --outfile {tmp_folder_participant}/{name_nii} {f} ')
+                    try:   
+                        sub.check_call(command, shell=True)
+                        print(f'deface nifti {name_nii} and copy in {tmp_folder_participant}')
+                    except:
+                        print('Error in defacing and copying nifti into temporary folder, please make sure FSL is installed')
+                #if json file available copy it. If not create a fake one
+                if len(files_json)>1:
+                    print('WARNING: There should be only one json file. Check again and remove additional file.')
+                elif len(files_json)==1:
+                    fjson = files_json[0] 
+                    command = format(f'cp {fjson} {tmp_folder_participant}/{name_json}')
+                    try:   
+                        sub.check_call(command, shell=True)
+                        print(f'just copy json {name_json} in {tmp_folder_participant}')
+                    except:
+                        print('Error in copying json into temporary folder')
+                else:
+                    print('Creation of a json file into temporary folder')
+                    info={}
+                    info['strength']=T
+                    json_file = os.path.join(tmp_folder_participant,name_json)
+                    create_json_file(json_file,info)
+            #if dwi modality, copy bval and bvec
+            if mod=='dwi':
+                if (len(files_bval)<1) or (len(files_bvec)<1):
+                    print(f'WARNING: missing files bvec and bvals for modality dwi. Please check files \
+                    are in the right folder {strenght}/{mod}')
+                else:
+                    name_bval = name_base+'.bval'
+                    name_bvec = name_base+'.bvec'
+                    fbval = files_bval[0]
+                    fbvec = files_bvec[0]
+                    command1 = format(f'cp {fbval} {tmp_folder_participant}/{name_bval}')
+                    command2 = format(f'cp {fbvec} {tmp_folder_participant}/{name_bvec}')
+                    try:   
+                        sub.check_call(command1, shell=True)
+                        print(f'just copy bval file {name_bval} in {tmp_folder_participant}')
+                        sub.check_call(command2, shell=True)
+                        print(f'just copy bvec file {name_bvec} in {tmp_folder_participant}')
+                    except:
+                        print('Error in copying bval or bvec into temporary folder')
+                    
+                    
+    return 
+
+    
+    
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Script to bidsify MELD data. PART 1 : convert DICOMs into NIFTI')
@@ -81,32 +164,44 @@ if __name__ == '__main__':
     parser.add_argument('-ids', '--list_participants',
                         help='Text file with participants ids to send',
                         required=True)
+    parser.add_argument('-njobs',
+                        help='precise number of cpu to use to run defacing in parallel. Default is all minus 1',
+                        default = -2)
     parser.add_argument('--nodeface',
                         help='if flag, do not deface the NIFTI images',
                         action="store_true")
     parser.add_argument('--nodel',
                         help='if flag, do not delete the meldbids folder after compression',
-                        action="store_true")
+                        action="store_true",)
+    parser.add_argument('-v','--verbose', 
+                        help='print issues',action='store_true',
+                         default=False, 
+                        )
     args = parser.parse_args()
 
     meld_folder= args.meld_folder
     list_participants = args.list_participants
-
+    QUIET =args.verbose
+    logging.basicConfig(level=logging.WARNING if QUIET else logging.INFO,
+                    format="%(message)s")
+    
     #information about meld_template 
     strenghts=['15T', '3T', '7T']
     modalities = ['t1','postop_t1', 't2', 'flair', 'dwi']
+    T_mod = list(itertools.product(*[strenghts,modalities]))
     
     #create a meldBIDS folder with today date
     now =  datetime.now()
     date = now.strftime("%d%m%Y")
-    bids_folder = os.path.join(meld_folder,'meld_bids',f'meldbids_{date}')
+    folder_name = f'meldbids_{date}'
+    bids_folder = os.path.join(meld_folder,'meld_bids',folder_name)
     if not os.path.exists(bids_folder):
             os.makedirs(bids_folder)
 
     #folder to store nifti and json file for dcm2bids
     tmp_folder = os.path.join(bids_folder, 'tmp_dcm2bids')
     if not os.path.exists(tmp_folder):
-            os.makedirs(tmp_folder)
+        os.makedirs(tmp_folder)
 
     #load participants from list 
     f = open(list_participants, 'r')
@@ -131,57 +226,11 @@ if __name__ == '__main__':
         #create the temporary folder of the participants
         tmp_folder_participant= os.path.join(tmp_folder, 'sub-' + participant_bids)
         if not os.path.exists(tmp_folder_participant):
-                os.makedirs(tmp_folder_participant)
+            os.makedirs(tmp_folder_participant)
         
-        #loop over each folder in meld_template
-        for T in strenghts:
-            for mod in modalities:
-                dcm_folder=os.path.join(path,T,mod)
-                if not os.path.isdir(dcm_folder):
-                    print(f'folder {dcm_folder} does not exist. Check your participant folder is similar to the meld_template')
-                if not os.listdir(dcm_folder):
-                    pass
-                else:
-                    files_nii=glob.glob(os.path.join(dcm_folder,'*.nii*'))
-                    files_json=glob.glob(os.path.join(dcm_folder,'*.json'))
-                    name_base = '.'.join([T,mod])
-                    name_nii = name_base+'.nii'
-                    name_json = name_base+'.json'
-                    #if nifti file : copy nifti in tmp_dcm2bids and create fake json file
-                    if len(files_nii)>1:
-                        print('WARNING: There should be only one nifti file. Check again and remove additional file.')
-                    elif len(files_nii)==1:
-                        f = files_nii[0] 
-                        if args.nodeface==True:
-                            command = format(f'cp {f} {tmp_folder_participant}/{name_nii}')
-                        else:
-                            command = format(f'pydeface --outfile {tmp_folder_participant}/{name_nii} {f} ')
-                        try:   
-                            sub.check_call(command, shell=True)
-                            print(f'deface nifti {name_nii} and copy in {tmp_folder_participant}')
-                        except:
-                            print('Error in defacing and copying nifti into temporary folder, please make sure FSL is installed')
-                        #if json file available copy it. If not create a fake one
-                        if len(files_json)>1:
-                            print('WARNING: There should be only one json file. Check again and remove additional file.')
-                        elif len(files_json)==1:
-                            f = files_json[0] 
-                            command = format(f'cp {f} {tmp_folder_participant}/{name_json}')
-                            try:   
-                                sub.check_call(command, shell=True)
-                                print(f'just copy json {name_json} in {tmp_folder_participant}')
-                            except:
-                                print('Error in copying json into temporary folder')
-                        else:
-                            print('Creation of a json file into temporary folder')
-                            info={}
-                            info['strength']=T
-                            json_file = os.path.join(tmp_folder_participant,name_json)
-                            create_json_file(json_file,info)
-                    else:
-                        pass
-
-
+        # Parallelise for each strenght_modality (n_jobs=1 means: use all available cores)
+        print(f'Parallelise defacing with {str(args.njobs)} jobs')
+        element_information = Parallel(n_jobs=int(args.njobs))(delayed(deface_and_sort_nii)(node, path, tmp_folder_participant) for node in T_mod)
         #convert in bids structure using dcm2bids
         config_file = os.path.join(meld_folder,'scripts','meld_dcm2bids_config.json')
         command=format(f'dcm2bids -d {participants_folder} -p {participant_bids} -c {config_file} -o {bids_folder}')
@@ -196,11 +245,11 @@ if __name__ == '__main__':
         
         #compression in batch
         print(f'Compress {bids_folder}')
-        command=format(f'zip -r {bids_folder} {bids_folder}; split -b 800M {bids_folder}.zip {bids_folder}/share_data_part')
+        command=format(f'cd {meld_folder}/meld_bids ; zip -r {folder_name} {folder_name} ; split -b 800M {folder_name}.zip share_data_part_')
         sub.check_call(command, shell=True)
         
-        #delete meldbids folder if flag
-        if args.nodel==True:
+        # do not delete meldbids folder if flag
+        if args.nodel==False:
             shutil.rmtree(bids_folder)
             
         print('End of STEP 2' )
